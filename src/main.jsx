d000676c81284ccecc17333ae20f00d91b8f8746
BayramliHumay <114084341+BayramliHumay@users.noreply.github.com>
2022-11-23 01:42:25 -0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Ders4 from './Component/Ders'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+ <div>
+  <Ders4/>
+
+
+ </div>
+  
+)
